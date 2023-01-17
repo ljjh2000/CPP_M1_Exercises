8047ac833bb72078835b0d29380183f3f83c8374
ljjh2000 <jacqueline93140@gmail.com>
2023-01-17 12:16:14 +0100

    add question 2-1

--- src/translator.cpp
+++ src/translator.cpp
@@ -1,11 +1,36 @@
 #include <iostream>
+#include <string>
+#include <set>
+
+using namespace std;
+
+set<string> make_exit_commands()
+{
+    return {"q", "e", "exit", "quit"};
+}
 
 int main()
 {
+
+    string tmp;
+
     while (true)
     {
-        std::cout << "This is a translator, but it does nothing for now..." << std::endl;
-        std::cin.ignore();
+        cout << "This is a translator, but it does nothing for now..." << endl;
+        cin >> tmp;
+        auto exit_commande = make_exit_commands();
+        if (auto search = exit_commande.find(tmp); search != exit_commande.end())
+        {
+            break;
+        }
+        if (tmp.compare("add") == 0)
+        {
+            string w1;
+            string w2;
+            cin >> w1;
+            cin >> w2;
+            cout << w1 + " => " + w2 << endl;
+        }
     }
 
     return 0;
