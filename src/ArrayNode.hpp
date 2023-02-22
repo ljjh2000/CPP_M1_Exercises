e1a1a2812d4ff50436f694c741b596b0929d9d1b
ljjh2000 <jacqueline93140@gmail.com>
2023-02-22 18:58:37 +0100

    q11

--- src/ArrayNode.hpp
+++ src/ArrayNode.hpp
@@ -18,11 +18,24 @@ public:
     {}
     ~ArrayNode() = default;
 
-    std::string print() const override { return "[]"; }
+    std::string print() const override
+    {
+        std::string result = "[";
+        for (size_t i = 0; i < _arrayNodes.size(); i++)
+        {
+            result += _arrayNodes[i]->print();
+            if (i != _arrayNodes.size() - 1)
+            {
+                result += ",";
+            }
+        }
+        result += "]";
+        return result;
+    }
 
     void push_back(NodePtr arrayNode) { _arrayNodes.emplace_back(std::move(arrayNode)); }
 
-    int child_count() const { return InstanceCounter::counter(); }
+    size_t child_count() const { return _arrayNodes.size(); }
 
     static std::unique_ptr<ArrayNode> make_ptr() { return std::make_unique<ArrayNode>(); }
 };
